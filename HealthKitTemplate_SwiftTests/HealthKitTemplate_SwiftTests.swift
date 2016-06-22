23dbb11317b197eac7e00bea147df71c13971e23
xramoli <xaviramos3@gmail.com>
2016-06-22 14:40:26 +0200

    Initial Commit

---
+++ HealthKitTemplate_SwiftTests/HealthKitTemplate_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  HealthKitTemplate_SwiftTests.swift
+//  HealthKitTemplate_SwiftTests
+//
+//  Created by Xavier Ramos Oliver on 22/06/16.
+//  Copyright © 2016 SenseHealth. All rights reserved.
+//
+
+import XCTest
+@testable import HealthKitTemplate_Swift
+
+class HealthKitTemplate_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
